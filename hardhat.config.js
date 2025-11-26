04fa84540f8e9cde9feace4d9d1f738e27b73507
jasonberba-vls <127728314+jasonberba-vls@users.noreply.github.com>
2025-11-26 17:55:13 +0400

    Additional Implementations

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,14 @@
-require('dotenv').config();
+require("dotenv").config();
 require("@nomiclabs/hardhat-ethers");
 
-const { RPC_URL, PRIVATE_KEY } = process.env;
+const {
+  ETH_RPC_URL,
+  ETH_PRIVATE_KEY,
+  BSC_RPC_URL,
+  BSC_PRIVATE_KEY,
+  POL_RPC_URL,
+  POL_PRIVATE_KEY,
+} = process.env;
 
 module.exports = {
   solidity: {
@@ -15,12 +22,29 @@ module.exports = {
       },
     ],
   },
-  defaultNetwork: "sepolia",
+  // defaultNetwork: "sepolia",
+  // defaultNetwork: "BscTestnet",
+  defaultNetwork: "PolTestnet",
+
   networks: {
     hardhat: {},
+
+    // Ethereum Sepolia
     sepolia: {
-      url: RPC_URL,
-      accounts: [`0x${PRIVATE_KEY}`]
-    }
+      url: ETH_RPC_URL,
+      accounts: [`${ETH_PRIVATE_KEY}`],
+    },
+
+    // BSC TESTNET
+    BscTestnet: {
+      url: BSC_RPC_URL,
+      accounts: [`${BSC_PRIVATE_KEY}`],
+    },
+
+    // POL TESTNET
+    PolTestnet: {
+      url: POL_RPC_URL,
+      accounts: [`${POL_PRIVATE_KEY}`],
+    },
   },
-}
+};
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,25 +1,34 @@
 // ***
 // --network : mainnet / sepolia
-// Command to execute script : 'npx hardhat run scripts/deploy.js --network sepolia'
+// Command to execute script for ETH : 'npx hardhat run scripts/deploy.js --network sepolia'
+// Command to execute script for BSC : 'npx hardhat run scripts/deploy.js --network BscTestnet'
 
 async function main() {
-    //Initialize Solidity Contract Object
-    const contractName = "MintErc20TokenContract";
-    const contract = await ethers.getContractFactory(contractName);
+  //Initialize Solidity Contract Object
+  const contractName = "MintErc20TokenContract";
+  const contract = await ethers.getContractFactory(contractName);
 
-    //Execute Token Minting
-    //Token Name : Test6USDT
-    //Token Code : T6USDT
-    //Decimal Precision : 6
-    //Token Amount : 10,000
-    const contractDeployResult = await contract.deploy('Test6USDT', 'T6USDT', 6, 10000);
-    console.log(`Contract ${contractName} Deployed to Address:`, contractDeployResult.address);
+  //Execute Token Minting
+  //Token Name : XurlinkUSDT
+  //Token Code : XUSDT
+  //Decimal Precision : 6
+  //Token Amount : 10,000,000
+  const contractDeployResult = await contract.deploy(
+    "XurlinkUSDT",
+    "XUSDT",
+    6,
+    10000000
+  );
+  console.log(
+    `Contract ${contractName} Deployed to Address:`,
+    contractDeployResult.address
+  );
 
-    //Sample created ERC20 Token in Seplia Network: 0x9f0c99e8B28d9cf19Ba94f9C60ba30032501b8E4
-  }
-  main()
-    .then(() => process.exit(0))
-    .catch(error => {
-      console.error(error);
-      process.exit(1);
-    });
+  //Sample created ERC20 Token in Seplia Network: 0x9f0c99e8B28d9cf19Ba94f9C60ba30032501b8E4
+}
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
